b1ecfd2191cb2be91d693459cc1c715a10b13d64
Arona Jones <warlordjones@gmail.com>
2014-05-24 20:11:22 +0100

    Dummy file, for now

---
+++ src/main/java/boilerplate/client/fx/FXRaygun.java
@@ -0,0 +1,14 @@
+package boilerplate.client.fx;
+
+import net.minecraft.client.particle.EntityFX;
+import net.minecraft.world.World;
+
+public class FXRaygun extends EntityFX
+{
+	//!!Had to move, for now
+	protected FXRaygun(World par1World, double par2, double par4, double par6)
+	{
+		super(par1World, par2, par4, par6);
+		// TODO Auto-generated constructor stub
+	}
+}
