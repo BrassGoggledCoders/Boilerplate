cbcd988f3c8478d4cf9f9690130ec721c49f4624
Arona Jones <warlordjones@gmail.com>
2014-05-31 17:21:19 +0100

    registerArmorSet method added to RegisteryHelper

--- src/main/java/boilerplate/common/RegistryHelper.java
+++ src/main/java/boilerplate/common/RegistryHelper.java
@@ -1,8 +1,9 @@
 package boilerplate.common;
 
-import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class RegistryHelper
 {
@@ -11,5 +12,12 @@ public class RegistryHelper
 		GameRegistry.registerBlock(block, name);
 		String id = "TE" + name.substring(5);
 		GameRegistry.registerTileEntity(tile, id);
+	}
+	public static void registerArmorSet(Item helm, Item chest, Item legs, Item boots, String name)
+	{
+		GameRegistry.registerItem(helm, "ItemHelmet" + name);
+		GameRegistry.registerItem(chest, "ItemChest" + name);
+		GameRegistry.registerItem(legs, "ItemLegs" + name);
+		GameRegistry.registerItem(boots, "ItemBoots" + name);
 	}
 }
