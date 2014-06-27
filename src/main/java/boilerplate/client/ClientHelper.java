ae865365ce0695c9dc5674738971f5713b709f83
Arona Jones <warlordjones@gmail.com>
2014-06-27 14:05:21 +0100

    Transfer over some client helper methods from SC2

--- src/main/java/boilerplate/client/ClientHelper.java
+++ src/main/java/boilerplate/client/ClientHelper.java
@@ -1,5 +1,8 @@
 package boilerplate.client;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityClientPlayerMP;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.util.EnumChatFormatting;
 
 import org.lwjgl.input.Keyboard;
@@ -9,5 +12,34 @@ public class ClientHelper {
 
 	public static boolean isShiftKeyDown() {
 		return (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) || (Keyboard.isKeyDown(Keyboard.KEY_RSHIFT));
+	}
+	/**
+	 * Mc.
+	 *
+	 * @return the minecraft
+	 */
+	public static Minecraft mc()
+	{
+		return Minecraft.getMinecraft();
+	}
+
+	/**
+	 * Font renderer.
+	 *
+	 * @return the font renderer
+	 */
+	public static FontRenderer fontRenderer()
+	{
+		return mc().fontRenderer;
+	}
+
+	/**
+	 * Client player.
+	 *
+	 * @return the entity client player mp
+	 */
+	public static EntityClientPlayerMP clientPlayer()
+	{
+		return mc().thePlayer;
 	}
 }
--- src/main/java/boilerplate/client/utils/EffectUtils.java
+++ src/main/java/boilerplate/client/utils/EffectUtils.java
@@ -20,6 +20,9 @@ import net.minecraft.client.particle.EntityFX;
 import net.minecraft.client.particle.EntityFlameFX;
 import net.minecraft.client.particle.EntitySmokeFX;
 import net.minecraft.world.World;
+
+import org.lwjgl.opengl.GL11;
+
 import cpw.mods.fml.client.FMLClientHandler;
 
 // TODO: Auto-generated Javadoc
@@ -187,6 +190,20 @@ public class EffectUtils
 		{
 			mc.effectRenderer.addEffect(particle);
 		}
+	}
+
+	/**
+	 * Sets the GL color from int.
+	 *
+	 * @param color
+	 *            the new GL color from int
+	*/
+	public static void setGLColorFromInt(int color)
+	{
+		float red = (color >> 16 & 255) / 255.0F;
+		float green = (color >> 8 & 255) / 255.0F;
+		float blue = (color & 255) / 255.0F;
+		GL11.glColor4f(red, green, blue, 1.0F);
 	}
 
 }
