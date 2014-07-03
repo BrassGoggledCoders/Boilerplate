f724cd5a0fb2f4694b07a04c61e1cb1df032e1f2
Decebaldecebal <rares_serban96@yahoo.com>
2014-07-03 22:15:51 +0300

    Updated to MC 1.7.10 and Forge 10.13.0.1160

--- src/main/java/boilerplate/common/utils/PlayerUtils.java
+++ src/main/java/boilerplate/common/utils/PlayerUtils.java
@@ -33,24 +33,30 @@ import net.minecraft.world.World;
 public class PlayerUtils
 {
 	// Grabs a vector from the player
-	public static MovingObjectPosition getTargetBlock(World world, Entity entity, boolean liquids)
+	public static MovingObjectPosition getTargetBlock(World world, Entity player, boolean par3, double range)
 	{
-		float var4 = 1.0F;
-		float var5 = entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * var4;
-		float var6 = entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * var4;
-		double var7 = entity.prevPosX + (entity.posX - entity.prevPosX) * var4;
-		double var9 = entity.prevPosY + (entity.posY - entity.prevPosY) * var4 + 1.62D - entity.yOffset;
-		double var11 = entity.prevPosZ + (entity.posZ - entity.prevPosZ) * var4;
-		Vec3 var13 = world.getWorldVec3Pool().getVecFromPool(var7, var9, var11);
-		float var14 = MathHelper.cos(-var6 * 0.01745329F - 3.141593F);
-		float var15 = MathHelper.sin(-var6 * 0.01745329F - 3.141593F);
-		float var16 = -MathHelper.cos(-var5 * 0.01745329F);
-		float var17 = MathHelper.sin(-var5 * 0.01745329F);
-		float var18 = var15 * var16;
-		float var20 = var14 * var16;
-		double var21 = 10.0D;
-		Vec3 var23 = var13.addVector(var18 * var21, var17 * var21, var20 * var21);
-		return world.rayTraceBlocks(var13, var23, liquids);
+		 float f = 1.0F;
+        float f1 = player.prevRotationPitch + (player.rotationPitch - player.prevRotationPitch) * f;
+        float f2 = player.prevRotationYaw + (player.rotationYaw - player.prevRotationYaw) * f;
+        double d0 = player.prevPosX + (player.posX - player.prevPosX) * (double) f;
+        double d1 = player.prevPosY + (player.posY - player.prevPosY) * (double) f;
+        if (!world.isRemote && player instanceof EntityPlayer)
+            d1 += 1.62D;
+        double d2 = player.prevPosZ + (player.posZ - player.prevPosZ) * (double) f;
+        Vec3 vec3 = Vec3.createVectorHelper(d0, d1, d2);
+        float f3 = MathHelper.cos(-f2 * 0.017453292F - (float) Math.PI);
+        float f4 = MathHelper.sin(-f2 * 0.017453292F - (float) Math.PI);
+        float f5 = -MathHelper.cos(-f1 * 0.017453292F);
+        float f6 = MathHelper.sin(-f1 * 0.017453292F);
+        float f7 = f4 * f5;
+        float f8 = f3 * f5;
+        double d3 = range;
+        if (player instanceof EntityPlayerMP)
+        {
+            d3 = ((EntityPlayerMP) player).theItemInWorldManager.getBlockReachDistance();
+        }
+        Vec3 vec31 = vec3.addVector((double) f7 * d3, (double) f6 * d3, (double) f8 * d3);
+        return world.func_147447_a(vec3, vec31, par3, !par3, par3);
 	}
 	/**
 	 * Hmm...I wonder what this method does? Indeed! It *does* spawn potatoes!
