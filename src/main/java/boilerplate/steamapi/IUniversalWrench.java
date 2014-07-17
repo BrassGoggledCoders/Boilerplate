fc91f70fad2f08f99f1460b4617bbe89de93bd26
Arona Jones <warlordjones@gmail.com>
2014-07-17 11:57:08 +0200

    Add and utilise BC Wrench API

--- src/main/java/boilerplate/steamapi/IUniversalWrench.java
+++ src/main/java/boilerplate/steamapi/IUniversalWrench.java
@@ -1,7 +1,9 @@
 package boilerplate.steamapi;
 
+import buildcraft.api.tools.IToolWrench;
+
 //Oooh, look at this mum! A simple interface for implementing many mods wrench apis! Only trouble is, I can't find them!
-public interface IUniversalWrench /*extends IWrench*/
+public interface IUniversalWrench extends IToolWrench
 {
 
 }
--- src/main/java/boilerplate/steamapi/UniversalWrench.java
+++ src/main/java/boilerplate/steamapi/UniversalWrench.java
@@ -1,8 +1,22 @@
 package boilerplate.steamapi;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import boilerplate.common.RootItem;
 //Reference implementation of IUniversalWrench
-public class UniversalWrench extends RootItem
+public class UniversalWrench extends RootItem implements IUniversalWrench
 {
+	@Override
+	public boolean canWrench(EntityPlayer player, int x, int y, int z)
+	{
+		return true;
+	}
+
+	@Override
+	public void wrenchUsed(EntityPlayer player, int x, int y, int z)
+	{
+		player.inventory.addItemStackToInventory(new ItemStack(player.worldObj.getBlock(x, y, z)));
+		player.worldObj.setBlockToAir(x, y, z);
+	}
 
 }
