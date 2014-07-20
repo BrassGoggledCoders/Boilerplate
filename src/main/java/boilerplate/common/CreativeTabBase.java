44452e4b57fa0e9866af58f296ab54f4b8b7ef45
Arona Jones <warlordjones@gmail.com>
2014-07-20 11:48:18 +0200

    Recive moved file from Steamcraft

---
+++ src/main/java/boilerplate/common/CreativeTabBase.java
@@ -0,0 +1,39 @@
+/**
+ * This class was created by BrassGoggledCoders modding team.
+ * This class is available as part of the Steamcraft 2 Mod for Minecraft.
+ *
+ * Steamcraft 2 is open-source and is distributed under the MMPL v1.0 License.
+ * (http://www.mod-buildcraft.com/MMPL-1.0.txt)
+ *
+ * Steamcraft 2 is based on the original Steamcraft Mod created by Proloe.
+ * Steamcraft (c) Proloe 2011
+ * (http://www.minecraftforum.net/topic/251532-181-steamcraft-source-code-releasedmlv054wip/)
+ *
+ */
+package boilerplate.common;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+/**
+ * @author warlordjones
+ *
+ */
+public class CreativeTabBase extends CreativeTabs
+{
+	Item icon;
+	public CreativeTabBase(int id, String name, Item icon)
+	{
+		super(id, name);
+		this.icon = icon;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Item getTabIconItem()
+	{
+		return icon;
+	}
+}
