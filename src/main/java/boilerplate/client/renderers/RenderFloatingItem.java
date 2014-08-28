32b8a15f0ca94c434f7d8245b9958cfeb284f394
Arona Jones <warlordjones@gmail.com>
2014-08-28 11:38:26 +0100

    Update RenderFloatingItem with working code

--- src/main/java/boilerplate/client/renderers/RenderFloatingItem.java
+++ src/main/java/boilerplate/client/renderers/RenderFloatingItem.java
@@ -8,9 +8,13 @@
  */
 package boilerplate.client.renderers;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
+
+import org.lwjgl.opengl.GL11;
 
 /**
  * @author warlordjones
@@ -18,30 +22,16 @@ import net.minecraft.entity.item.EntityItem;
  */
 public class RenderFloatingItem extends RenderItem
 {
-	private boolean bob;
-	private boolean spread;
-
-	public RenderFloatingItem(boolean bob, boolean spread)
+	public static void render(double dx, double dy, double dz, ItemStack stack)
 	{
-		this.bob = bob;
-		this.spread = spread;
-		this.setRenderManager(RenderManager.instance);
-	}
-
-	public void render(EntityItem item, float x, float y, float z)
-	{
-		this.doRender(item, x, y, z, 0, 0);
-	}
-
-	@Override
-	public boolean shouldBob()
-	{
-		return this.bob;
-	}
-
-	@Override
-	public boolean shouldSpreadItems()
-	{
-		return this.spread;
+		EntityItem entItem = new EntityItem(Minecraft.getMinecraft().thePlayer.getEntityWorld(), 0D, 0D, 0D, stack);
+		//Without the below line, the item will spazz out
+		entItem.hoverStart = 0.0F;
+		RenderItem.renderInFrame = true;
+		GL11.glTranslatef((float)dx + 0.5F, (float)dy + 0.3F, (float)dz + 0.3F);
+		GL11.glRotatef(180, 0, 5.5F, 1);
+		GL11.glScalef(1.6F, 1.6F, 1.6F);
+		RenderManager.instance.renderEntityWithPosYaw(entItem, 0.0D, 0.0D, 0.0D, 0.0F, 0.0F);
+		RenderItem.renderInFrame = false;
 	}
 }
