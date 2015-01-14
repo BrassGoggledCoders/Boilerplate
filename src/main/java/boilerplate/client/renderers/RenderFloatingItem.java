48ca1a37df2764dee572e4fa5c60df3c1f4e7402
Arona Jones <warlordjones@gmail.com>
2015-01-14 19:39:30 +0000

    Add an option for scale in RenderFloatingItem

--- src/main/java/boilerplate/client/renderers/RenderFloatingItem.java
+++ src/main/java/boilerplate/client/renderers/RenderFloatingItem.java
@@ -36,4 +36,20 @@ public class RenderFloatingItem extends RenderItem
 		RenderManager.instance.renderEntityWithPosYaw(entItem, 0.0D, 0.0D, 0.0D, 0.0F, 0.0F);
 		RenderItem.renderInFrame = false;
 	}
+
+	public static void render(double dx, double dy, double dz, float rotX, float rotY, float rotZ, float scaleX, float scaleY, float scaleZ,
+			ItemStack stack)
+	{
+		EntityItem entItem = new EntityItem(Minecraft.getMinecraft().thePlayer.getEntityWorld(), 0D, 0D, 0D, stack);
+		// Without the below line, the item will spazz out
+		entItem.hoverStart = 0.0F;
+		RenderItem.renderInFrame = true;
+		GL11.glTranslatef((float) dx, (float) dy, (float) dz);
+		GL11.glRotatef(rotX, 1, 0, 0);
+		GL11.glRotatef(rotY, 0, 1, 0);
+		GL11.glRotatef(rotZ, 0, 0, 1);
+		GL11.glScalef(scaleX, scaleY, scaleZ);
+		RenderManager.instance.renderEntityWithPosYaw(entItem, 0.0D, 0.0D, 0.0D, 0.0F, 0.0F);
+		RenderItem.renderInFrame = false;
+	}
 }
