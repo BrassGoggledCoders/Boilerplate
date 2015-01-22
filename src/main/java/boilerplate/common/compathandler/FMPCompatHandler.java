00ac5396b8e396e64e66485c339a1f800bd527d3
Arona Jones <warlordjones@gmail.com>
2015-01-22 20:59:59 +0000

    Meta FMP Block handler.

--- src/main/java/boilerplate/common/compathandler/FMPCompatHandler.java
+++ src/main/java/boilerplate/common/compathandler/FMPCompatHandler.java
@@ -1,6 +1,9 @@
 package boilerplate.common.compathandler;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
@@ -13,15 +16,32 @@ import cpw.mods.fml.common.event.FMLInterModComms;
 public class FMPCompatHandler
 {
 	static ArrayList<Block> blocksToRegister = new ArrayList();
+	static Map<Block, Integer> metaBlocksToRegister = new HashMap();
 
 	public static void registerFMP(Block block)
 	{
 		blocksToRegister.add(block);
 	}
 
+	public static void registerFMP(Block block, int maxMeta)
+	{
+		metaBlocksToRegister.put(block, maxMeta);
+	}
+
 	public static void doRegister()
 	{
 		for (int i = 0; i < blocksToRegister.size(); i++)
 			FMLInterModComms.sendMessage("ForgeMicroblock", "microMaterial", new ItemStack(blocksToRegister.get(i), 1));
+	}
+
+	public static void doRegisterMeta()
+	{
+		Iterator i = metaBlocksToRegister.entrySet().iterator();
+		while (i.hasNext())
+		{
+			Map.Entry pairs = (Map.Entry) i.next();
+			FMLInterModComms.sendMessage("ForgeMicroblock", "microMaterial", new ItemStack((Block) pairs.getKey(), 1, (int) pairs.getValue()));
+			i.remove();
+		}
 	}
 }
