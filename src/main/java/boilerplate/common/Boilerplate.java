64679411e16113ed085fbf91707212d9b045c3df
Arona Jones <warlordjones@gmail.com>
2015-01-25 21:02:50 +0000

    Bump version for new hotfix

--- src/main/java/boilerplate/common/Boilerplate.java
+++ src/main/java/boilerplate/common/Boilerplate.java
@@ -21,7 +21,7 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
  * @author Surseance
  * 
  */
-@Mod(modid = "boilerplate", name = "Boilerplate", version = "3.0.5", dependencies = "after:BuildCraft|Core")
+@Mod(modid = "boilerplate", name = "Boilerplate", version = "3.0.5c", dependencies = "after:BuildCraft|Core")
 public class Boilerplate
 {
 	@SidedProxy(clientSide = "boilerplate.client.ClientProxy", serverSide = "boilerplate.common.CommonProxy")
