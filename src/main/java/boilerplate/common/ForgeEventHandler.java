f0b84deae9e45960443dff07279db91c0e86e3ef
Arona Jones <warlordjones@gmail.com>
2015-01-27 22:01:32 +0000

    Make flametrail clientside only for now

--- src/main/java/boilerplate/common/ForgeEventHandler.java
+++ src/main/java/boilerplate/common/ForgeEventHandler.java
@@ -6,22 +6,19 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
 public class ForgeEventHandler
 {
 	// TODO There must be a more efficient way to handle this!
 	@SubscribeEvent
-	@SideOnly(Side.CLIENT)
 	public void tickPlayer(LivingUpdateEvent event)
 	{
-		if (event.entityLiving instanceof EntityPlayer)
+		if (event.entityLiving instanceof EntityPlayer && event.entityLiving.worldObj.isRemote)
 		{
+			World world = event.entityLiving.worldObj;
 			EntityPlayer player = (EntityPlayer) event.entityLiving;
 			if (Arrays.asList(Boilerplate.donors).contains(player.getCommandSenderName()))
 			{
-				World world = player.worldObj;
 				for (int i = 0; i < 10; i++)
 					world.spawnParticle("flame", player.posX + world.rand.nextDouble() - 0.5D, player.posY + world.rand.nextDouble() - 3D,
 							player.posZ + world.rand.nextDouble() - 0.5D, -player.motionX, -player.motionY, -player.motionZ);
