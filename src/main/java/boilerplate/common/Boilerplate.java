54f0ba0b6b2537f6c36e8d65cc826246c5f5938d
Arona Jones <warlordjones@gmail.com>
2015-01-27 21:06:42 +0000

    Donors get a pretty neat flametrail where they walk

--- src/main/java/boilerplate/common/Boilerplate.java
+++ src/main/java/boilerplate/common/Boilerplate.java
@@ -8,6 +8,7 @@
  */
 package boilerplate.common;
 
+import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -24,6 +25,8 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 @Mod(modid = "boilerplate", name = "Boilerplate", version = "3.0.5c", dependencies = "after:BuildCraft|Core")
 public class Boilerplate
 {
+	public static String[] donors = { "warlordjones"/* Donate PLZ */};
+
 	@SidedProxy(clientSide = "boilerplate.client.ClientProxy", serverSide = "boilerplate.common.CommonProxy")
 	public static CommonProxy proxy;
 
@@ -31,14 +34,15 @@ public class Boilerplate
 	public static Boilerplate instance;
 
 	@EventHandler
-	public void preInit(final FMLPreInitializationEvent event)
+	public void preInit(FMLPreInitializationEvent event)
 	{
-		// MinecraftForge.EVENT_BUS.register(ForgeEventHandler.class);
+
 	}
 
 	@EventHandler
 	public void init(FMLInitializationEvent event)
 	{
+		MinecraftForge.EVENT_BUS.register(new ForgeEventHandler());
 		proxy.registerRenderHandlers();
 	}
 
--- src/main/java/boilerplate/common/ForgeEventHandler.java
+++ src/main/java/boilerplate/common/ForgeEventHandler.java
@@ -1,19 +1,31 @@
 package boilerplate.common;
 
+import java.util.Arrays;
+
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ForgeEventHandler
 {
+	// TODO There must be a more efficient way to handle this!
 	@SubscribeEvent
-	public void onPlayerUseItem(PlayerUseItemEvent.Tick event)
+	@SideOnly(Side.CLIENT)
+	public void tickPlayer(LivingUpdateEvent event)
 	{
-		// if (event.item.getItem() == Items.potionitem &&
-		// event.item.getItemDamage() == 8238)
+		if (event.entityLiving instanceof EntityPlayer)
 		{
-			EntityPlayer player = event.entityPlayer;
-			player.worldObj.playAuxSFXAtEntity(player, 2000, player.serverPosX, player.serverPosY, player.serverPosZ, 4);
+			EntityPlayer player = (EntityPlayer) event.entityLiving;
+			if (Arrays.asList(Boilerplate.donors).contains(player.getCommandSenderName()))
+			{
+				World world = player.worldObj;
+				for (int i = 0; i < 10; i++)
+					world.spawnParticle("flame", player.posX + world.rand.nextDouble() - 0.5D, player.posY + world.rand.nextDouble() - 3D,
+							player.posZ + world.rand.nextDouble() - 0.5D, -player.motionX, -player.motionY, -player.motionZ);
+			}
 		}
 	}
 }
