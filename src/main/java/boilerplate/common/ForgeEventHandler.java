6497fcae2e455b5f00ffe6893630497852975e17
Arona Jones <warlordjones@gmail.com>
2015-01-28 11:06:26 +0000

    Much reduce the numbers of particles in trails

--- src/main/java/boilerplate/common/ForgeEventHandler.java
+++ src/main/java/boilerplate/common/ForgeEventHandler.java
@@ -20,14 +20,14 @@ public class ForgeEventHandler
 			EntityPlayer player = event.player;
 			if (Arrays.asList(Boilerplate.donors).contains(player.getCommandSenderName()))
 			{
-				for (int i = 0; i < 10; i++)
+				for (int i = 0; i < 3; i++)
 					world.spawnParticle("iconcrack_" + Item.getIdFromItem(Items.gold_ingot), player.posX + world.rand.nextDouble() - 0.5D,
 							player.posY + world.rand.nextDouble() - 2D, player.posZ + world.rand.nextDouble() - 0.5D, -player.motionX,
 							-player.motionY, -player.motionZ);
 			}
 			if (Arrays.asList(Boilerplate.devs).contains(player.getCommandSenderName()))
 			{
-				for (int i = 0; i < 10; i++)
+				for (int i = 0; i < 3; i++)
 					world.spawnParticle("flame", player.posX + world.rand.nextDouble() - 0.5D, player.posY + world.rand.nextDouble() - 3D,
 							player.posZ + world.rand.nextDouble() - 0.5D, -player.motionX, -player.motionY, -player.motionZ);
 			}
