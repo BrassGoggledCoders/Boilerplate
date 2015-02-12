45eb6a7cb7c1a244817b22f6d1b7233a2ad6054f
Gerard Bruwn <siribby@outlook.com>
2015-02-11 21:41:02 -0800

    Better ore registration

--- src/main/java/boilerplate/common/utils/helpers/OreDictHelper.java
+++ src/main/java/boilerplate/common/utils/helpers/OreDictHelper.java
@@ -8,6 +8,8 @@
  */
 package boilerplate.common.utils.helpers;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
@@ -17,7 +19,37 @@ import net.minecraftforge.oredict.OreDictionary;
  */
 public class OreDictHelper
 {
-	public static void registerOreWithAlts(String[] names, ItemStack ore)
+	public static void registerOre(String name, Block ore, int meta)
+	{
+		OreDictionary.registerOre(name, new ItemStack(ore, 1, meta));
+	}
+
+	public static void registerOre(String name, Item ore, int meta)
+	{
+		OreDictionary.registerOre(name, new ItemStack(ore, 1, meta));
+	}
+
+	public static void registerOreWithAlts(Block ore, String... names)
+	{
+		registerOreWithAlts(ore, 0, names);
+	}
+
+    public static void registerOreWithAlts(Block ore, int meta, String... names)
+    {
+        registerOreWithAlts(new ItemStack(ore, 1, meta), names);
+    }
+
+	public static void registerOreWithAlts(Item ore, String... names)
+	{
+		registerOreWithAlts(ore, 0, names);
+	}
+
+    public static void registerOreWithAlts(Item ore, int meta, String... names)
+    {
+        registerOreWithAlts(new ItemStack(ore, 1, meta), names);
+    }
+
+	public static void registerOreWithAlts(ItemStack ore, String... names)
 	{
 		for (String name : names)
 			OreDictionary.registerOre(name, ore);
