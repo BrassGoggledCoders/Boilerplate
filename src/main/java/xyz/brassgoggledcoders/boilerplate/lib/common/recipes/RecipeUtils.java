b54422356add561f83565aebe14407967265b281
Skylar Sommers <Skylar.Som@gmail.com>
2016-02-28 19:27:37 -0500

    Removed unnecessary Recipe stuff

--- src/main/java/xyz/brassgoggledcoders/boilerplate/lib/common/recipes/RecipeUtils.java
+++ src/main/java/xyz/brassgoggledcoders/boilerplate/lib/common/recipes/RecipeUtils.java
@@ -31,37 +31,4 @@ public class RecipeUtils
 		CustomMetalRecipes customMetalRecipes = new CustomMetalRecipes(block, ingot, nugget, meta);
 		customMetalRecipes.addRecipes(CraftingManager.getInstance());
 	}
-
-	public static void addMinecartRecipe(ItemStack resultItemStack, ItemStack cartBlock)
-	{
-		GameRegistry.addShapelessRecipe(resultItemStack, cartBlock, Items.minecart);
-	}
-
-	/**
-	 * @author warlordjones
-	 *
-	 */
-	public static class CustomToolRecipes
-	{
-		private static String[][] recipePatterns = new String[][] { { "XXX", " # ", " # " }, { "X", "#", "#" }, { "XX", "X#", " #" },
-				{ "XX", " #", " #" }, { "X", "X", "#" } };
-
-		public ItemStack input;
-		public ItemStack[] outputs;
-
-		public CustomToolRecipes(ItemStack input, ItemStack[] outputs)
-		{
-			this.input = input;
-			this.outputs = outputs;
-		}
-
-		public void addRecipes(final CraftingManager manager)
-		{
-			manager.addRecipe(this.outputs[0], recipePatterns[0], '#', Items.stick, 'X', this.input);
-			manager.addRecipe(this.outputs[1], recipePatterns[1], '#', Items.stick, 'X', this.input);
-			manager.addRecipe(this.outputs[2], recipePatterns[2], '#', Items.stick, 'X', this.input);
-			manager.addRecipe(this.outputs[3], recipePatterns[3], '#', Items.stick, 'X', this.input);
-			manager.addRecipe(this.outputs[4], recipePatterns[4], '#', Items.stick, 'X', this.input);
-		}
-	}
 }
