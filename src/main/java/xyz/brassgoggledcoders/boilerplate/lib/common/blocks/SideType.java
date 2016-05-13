436a14ade79395aac087b1151882d802a702393e
Skylar Sommers <ssommers@criticalmix.com>
2016-05-12 22:17:55 -0400

    Added SideType Properties

--- src/main/java/xyz/brassgoggledcoders/boilerplate/lib/common/blocks/SideType.java
+++ src/main/java/xyz/brassgoggledcoders/boilerplate/lib/common/blocks/SideType.java
@@ -1,9 +1,17 @@
 package xyz.brassgoggledcoders.boilerplate.lib.common.blocks;
 
+import net.minecraft.util.IStringSerializable;
+
 /**
  * @author SkySom
  */
-public enum SideType
+public enum SideType implements IStringSerializable
 {
-	INPUT, OUTPUT, NONE
+	INPUT, OUTPUT, NONE;
+
+	@Override
+	public String getName()
+	{
+		return this.toString();
+	}
 }
