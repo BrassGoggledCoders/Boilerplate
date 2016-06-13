a9363b7ce06e4af0424c3e2c6d0677833b8b8aa3
warlordjones <warlordjones@gmail.com>
2016-06-13 13:46:13 +0100

    That can't go there...

--- src/main/java/xyz/brassgoggledcoders/boilerplate/blocks/BlockSubBase.java
+++ src/main/java/xyz/brassgoggledcoders/boilerplate/blocks/BlockSubBase.java
@@ -1,23 +1,14 @@
 package xyz.brassgoggledcoders.boilerplate.blocks;
 
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.IBlockState;
 
 public class BlockSubBase extends BlockBase {
 
-	public static PropertyEnum<?> type;
-	
-	public BlockSubBase(Material mat, PropertyEnum<?> type) {
+	public BlockSubBase(Material mat) {
 		super(mat);
-		BlockSubBase.type = type;
 	}
-
-	@Override
-	public int getMetaFromState(IBlockState state) {
-		return ((IBlockType) state.getValue(type)).getMeta();
-	}
-
+	
 	@Override
 	public int damageDropped(IBlockState state) {
 		return getMetaFromState(state);
