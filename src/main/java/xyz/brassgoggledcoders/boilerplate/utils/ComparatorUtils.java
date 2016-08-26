27c7d6400c92702f0183d148e9d6655812071ed5
warlordjones <warlordjones@gmail.com>
2016-08-26 23:32:27 +0100

    Remove RF api and related, for neatness

--- src/main/java/xyz/brassgoggledcoders/boilerplate/utils/ComparatorUtils.java
+++ src/main/java/xyz/brassgoggledcoders/boilerplate/utils/ComparatorUtils.java
@@ -1,6 +1,5 @@
 package xyz.brassgoggledcoders.boilerplate.utils;
 
-import cofh.api.energy.IEnergyHandler;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.IFluidHandler;
@@ -9,18 +8,13 @@ import net.minecraftforge.fluids.IFluidHandler;
  * @author SkySom
  */
 public class ComparatorUtils {
-	public static int scaleStoredEnergyTo(int scale, IEnergyHandler energyHandler) {
-		return scaleStoredEnergyTo(scale, energyHandler, null);
-	}
 
-	public static int scaleStoredEnergyTo(int scale, IEnergyHandler energyHandler, EnumFacing direction) {
-		return scaleTo(scale, energyHandler.getEnergyStored(direction), energyHandler.getMaxEnergyStored(direction));
-	}
-
+	// TODO
 	public static int scaleSingleFluidLevelTo(int scale, IFluidHandler fluidHandler) {
 		return scaleSingleFluidLevelTo(scale, fluidHandler, null);
 	}
 
+	// TODO
 	public static int scaleSingleFluidLevelTo(int scale, IFluidHandler fluidHandler, EnumFacing direction) {
 		if(fluidHandler.getTankInfo(direction) != null && fluidHandler.getTankInfo(direction)[0] != null) {
 			FluidStack fluidStack = fluidHandler.getTankInfo(direction)[0].fluid;
