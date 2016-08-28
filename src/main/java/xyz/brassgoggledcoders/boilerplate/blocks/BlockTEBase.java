b87f4e662a602f8f7fe9a6bf862c72ad453c9e12
Skylar Sommers <ssommers@criticalmix.com>
2016-08-28 14:11:27 -0400

    Fixed not having the right method bubble through to extending classes

--- src/main/java/xyz/brassgoggledcoders/boilerplate/blocks/BlockTEBase.java
+++ src/main/java/xyz/brassgoggledcoders/boilerplate/blocks/BlockTEBase.java
@@ -1,13 +1,13 @@
 package xyz.brassgoggledcoders.boilerplate.blocks;
 
-import javax.annotation.Nonnull;
-
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
 
 public abstract class BlockTEBase extends BlockBase implements IHasTileEntity, ITileEntityProvider {
 	public BlockTEBase(Material material, String name) {
@@ -16,9 +16,9 @@ public abstract class BlockTEBase extends BlockBase implements IHasTileEntity, I
 	}
 
 	@Override
-	public void breakBlock(@Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
-		super.breakBlock(worldIn, pos, state);
-		worldIn.removeTileEntity(pos);
+	public void breakBlock(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
+		super.breakBlock(world, pos, state);
+		world.removeTileEntity(pos);
 	}
 
 	@SuppressWarnings("deprecation")
@@ -28,4 +28,14 @@ public abstract class BlockTEBase extends BlockBase implements IHasTileEntity, I
 		TileEntity tileentity = world.getTileEntity(pos);
 		return tileentity != null && tileentity.receiveClientEvent(id, param);
 	}
+
+	@Override
+	@Nonnull
+	public TileEntity createNewTileEntity(@Nonnull World world, int meta) {
+		return createTileEntity(world, getStateFromMeta(meta));
+	}
+
+	@Override
+	@Nonnull
+	public abstract TileEntity createTileEntity(@Nonnull World world, @Nonnull IBlockState blockState);
 }
